89f907e653d0b6efa39a9065e21ed52c9bbf80d4
clifton <clifga@gmail.com>
2023-10-01 00:40:45 -0400

    update

--- server/routes/content.py
+++ server/routes/content.py
@@ -1,18 +1,16 @@
 from flask_restful import Resource
 from models.models import Content, UserContentCart, User
-from config import api, db, request, make_response, abort, session, jsonify
+from config import api, db, request, make_response, jsonify
 
 
 class ContentList(Resource):
-    def get(self):
+    def get(self): #get a list of all content
         content = [content.to_dict() for content in Content.query.all()]
         return jsonify(content)
     
-    def post(self):
-
+    def post(self): #add a new content to the database
         form_json = request.get_json()
         new_content = Content(title=form_json['title'], video=form_json['video'], description=form_json['description'])
-        
         db.session.add(new_content)
         db.session.commit()
         response = make_response(
@@ -20,5 +18,22 @@ class ContentList(Resource):
             201
         )
         return response
+
+class ContentByID(Resource):
+    def get(self, id): #get a specific content by id
+        content = Content.query.filter_by(id=id).first()
+        return jsonify(content.to_dict())
     
+    def delete(self, id): #delete a specific content by id
+        content = Content.query.filter_by(id=id).first()
+        db.session.delete(content)
+        db.session.commit()
+        response = make_response(
+            content.to_dict(),
+            200
+        )
+        return response
+
 api.add_resource(ContentList, '/api/content')
+api.add_resource(ContentByID, '/api/content/<int:id>')
+
