0ded92d3aa685975b6cea4a95fd2150d9dd919fc
Jianqiu Wang <jianqiuwang@Jianqius-MBP.lan>
2023-10-03 15:29:56 -0500

    Initial complete implementation of ChatGPT chatbot

--- server/app.py
+++ server/app.py
@@ -1,24 +1,30 @@
+# app.py
+from flask import Flask, request, jsonify
+from flask_cors import CORS  # Import the CORS library
+import requests
+from dotenv import load_dotenv
+import os
 
-from flask import render_template
-from config import app, jsonify
-from routes.routes import *
+app = Flask(__name__)
+CORS(app)  # Initialize CORS with your app instance
 
-# Import your other modules here (models, routes, etc.)
+load_dotenv()
+# Load your API key from an environment variable or secret management service
+OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
 
-# Routes to render the "index.html" template
+@app.route('/api/chat', methods=['POST'])
+def chat():
+    payload = request.json
+    headers = {
+        "Authorization": f"Bearer {OPENAI_API_KEY}",
+        "Content-Type": "application/json"
+    }
+    response = requests.post(
+        "https://api.openai.com/v1/chat/completions",
+        headers=headers,
+        json=payload
+    )
+    return jsonify(response.json())
 
-@app.route('/api/users')
-def api_users():
-    # Add logic here to handle API requests if needed
-    return jsonify({"message": "API endpoint for users"})
-
-@app.route('/')
-@app.route('/home')
-@app.route('/auth')
-@app.route('/useronly')
-
-def index(id=0):
-    return render_template("index.html")
 if __name__ == '__main__':
-    # socketio.run(app, host='0.0.0.0', port=8000, debug=False)
-    app.run(port=5555, debug=True)
+    app.run(port=5000)  # will run on http://localhost:5000
