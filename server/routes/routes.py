cce4a49907727e5cbf263d0582e2ffaba2c1e51b
Sofia Katzman <katzmansof@gmail.com>
2023-10-04 06:14:53 -0400

    reconfigure for deployment

--- server/routes/routes.py
+++ server/routes/routes.py
@@ -1,5 +1,19 @@
 from .user import *
 from .auth import *
 from .content import *
+from config import OPENAI_API_KEY, requests, app, request
 
-# react front end routes 
+# chat bot route
+@app.route('/api/chat', methods=['POST'])
+def chat():
+    payload = request.json
+    headers = {
+        "Authorization": f"Bearer {OPENAI_API_KEY}",
+        "Content-Type": "application/json"
+    }
+    response = requests.post(
+        "https://api.openai.com/v1/chat/completions",
+        headers=headers,
+        json=payload
+    )
+    return jsonify(response.json())
